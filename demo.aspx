0027c9514ab3748c6bf3387bde5da788a96b3e7e
blackcatrecycler <1121042683@qq.com>
2016-12-09 09:27:18 +0800

    Add some note(0.0)

--- demo.aspx
+++ demo.aspx
@@ -16,13 +16,19 @@
         <asp:Repeater ID="Repeater1" runat="server">
             <ItemTemplate>
                 <il class="list">
+                    <%--这是一个标记位，表明这条状态是个啥--%>
+                    
                     <input type="hidden" class="mark" value="<%#Eval("mark")%>" />
+                    <%--日志的div显示日志的基本内容--%>
+                    <%--style="display:none" ----- 块隐藏     style="display:block" ------ 块显示--%>
                     <div class="blog" style="display:none">
                         日志:<%#Eval("title")%> <%#Eval("createtime")%>
                     </div>
+                    <%--照片的div显示照片的基本内容--%>
                     <div class="photo" style="display:none">
                         <img src="<%#Eval("imgpath")%>" /> <%#Eval("createtime")%>
                     </div>
+                    <%--说说的div显示说说的基本内容--%>
                     <div class="talk" style="display:none">
                         说说：<%#Eval("talkbody")%> <%#Eval("createtime")%>
                     </div>
@@ -32,19 +38,21 @@
             </ul>
     </div>
         <script>
-            var mark = $(".mark");
-            var blog = $(".blog");
-            var photo = $(".photo");
-            var talk = $(".talk");
-            for (var i = 0; i < mark.length; i++) {
-                if (mark.eq(i).val() == '0') {
-                    blog.eq(i).attr('style', 'display:blocl');
+            var mark = $(".mark");//所有的标记位-------*代码意义：所有class=mark的标签
+            var blog = $(".blog");//所有的日志div
+            var photo = $(".photo");//所有的照片div
+            var talk = $(".talk");//所有的说说div
+            for (var i = 0; i < mark.length; i++) {//遍历对象数组mark
+                if (mark.eq(i).val() == '0') {      //判断标志位---------*代码意义：mark.eq(i)----mark数组中第i个元素，即第i个class=mark的标签。
+                    //mark.eq(i).val()----元素的值，对应该标签就是value。
+                    blog.eq(i).attr('style', 'display:block');//如果标志位表明是日志，就把日志的块改为显示
+                    //blog.eq(i).attr('propertyName','value')  将元素blog.eq(i)的propertyName属性的值改为value
                 }
                 if (mark.eq(i).val() == '1') {
-                    photo.eq(i).attr('style', 'display:blocl');
+                    photo.eq(i).attr('style', 'display:block');//如果标志位表明是照片，就把照片的块改为显示
                 }
                 if (mark.eq(i).val() == '2') {
-                    talk.eq(i).attr('style', 'display:blocl');
+                    talk.eq(i).attr('style', 'display:block');//如果标志位表明是说说，就把说说的块改为显示
                 }
             }
         </script>
--- demo.aspx.cs
+++ demo.aspx.cs
@@ -20,14 +20,26 @@ public partial class demo : System.Web.UI.Page
             DataTable photodt = admin.DataT(strphoto);
             DataTable talkdt = admin.DataT(strtalk);
 
-            DataTable dt = new DataTable();
-            DataColumn dc1 = new DataColumn("mark", Type.GetType("System.String"));
+            //dt表结构
+            //| 所属 | 列名 | 作用 |
+            //| 共有 | mark | 标记 |
+            //| 共有 | id | 动态的id |
+            //| blog | title | 日志标题 |
+            //| blog | body | 日志主体 |
+            //| photo | imgpath | 照片的路径 |
+            //| talk | talkbody | 说说的内容 |
+            //| 共有 | createtime | 创建时间 |
+
+            DataTable dt = new DataTable();//创建一个新表
+            /*规定每列的列名，清楚地告诉dt你需要哪些内容*/
+            DataColumn dc1 = new DataColumn("mark", Type.GetType("System.String"));//因为只需要判断是否显示，所以将列的类型都设为string即可
             DataColumn dc2 = new DataColumn("id", Type.GetType("System.String"));
             DataColumn dc3 = new DataColumn("title", Type.GetType("System.String"));
             DataColumn dc4 = new DataColumn("body", Type.GetType("System.String"));
             DataColumn dc5 = new DataColumn("imgpath", Type.GetType("System.String"));
             DataColumn dc6 = new DataColumn("talkbody", Type.GetType("System.String"));
             DataColumn dc7 = new DataColumn("createtime", Type.GetType("System.String"));
+            /*将规定好的列填加进表中*/
             dt.Columns.Add(dc1);
             dt.Columns.Add(dc2);
             dt.Columns.Add(dc3);
@@ -35,10 +47,12 @@ public partial class demo : System.Web.UI.Page
             dt.Columns.Add(dc5);
             dt.Columns.Add(dc6);
             dt.Columns.Add(dc7);
+            /*到此就建好了一张完美的空表，下面开始填充数据*/
+            /*将blog表中的数据填进dt*/
             for(int i = 0; i < blogdt.Rows.Count; i++)
             {
                 DataRow dr = dt.NewRow();
-                dr["mark"] = "0";
+                dr["mark"] = "0";//日志标志位0
                 dr["id"] = blogdt.Rows[i][0].ToString();
                 dr["title"] = blogdt.Rows[i][1].ToString();
                 dr["body"] = blogdt.Rows[i][2].ToString();
@@ -47,10 +61,11 @@ public partial class demo : System.Web.UI.Page
                 dr["createtime"] = blogdt.Rows[i][3].ToString();
                 dt.Rows.Add(dr);
             }
+            /*将photo表中的数据填进dt*/
             for (int i = 0; i < photodt.Rows.Count; i++)
             {
                 DataRow dr = dt.NewRow();
-                dr["mark"] = "1";
+                dr["mark"] = "1";//日志标志位1
                 dr["id"] = photodt.Rows[i][0].ToString();
                 dr["title"] = "0";
                 dr["body"] = "0";
@@ -59,10 +74,11 @@ public partial class demo : System.Web.UI.Page
                 dr["createtime"] = photodt.Rows[i][2].ToString();
                 dt.Rows.Add(dr);
             }
+            /*将talk表中的数据填进dt*/
             for (int i = 0; i < talkdt.Rows.Count; i++)
             {
                 DataRow dr = dt.NewRow();
-                dr["mark"] = "2";
+                dr["mark"] = "2";//日志标志位2
                 dr["id"] = talkdt.Rows[i][0].ToString();
                 dr["title"] = "0";
                 dr["body"] = "0";
@@ -71,8 +87,8 @@ public partial class demo : System.Web.UI.Page
                 dr["createtime"] = talkdt.Rows[i][2].ToString();
                 dt.Rows.Add(dr);
             }
-            dt.DefaultView.Sort="createtime DESC";
-            Repeater1.DataSource = dt;
+            dt.DefaultView.Sort="createtime DESC";//给dt排个序
+            Repeater1.DataSource = dt;//将数据表作为repeater的数据源
             Repeater1.DataBind();
 
 
